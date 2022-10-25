223536e02aa9fa725297d9a6792ab7d5a66359c6
mjfp000 <mjfp000@outlook.com>
2022-10-25 11:32:03 +0200

    Cambios estéticos.

--- PRIMER_PROYECTO_WEB/src/main/java/Formulario.java
+++ PRIMER_PROYECTO_WEB/src/main/java/Formulario.java
@@ -12,46 +12,50 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/Formulario")
 public class Formulario extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public Formulario() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public Formulario() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		response.getWriter().append("Served at: ").append(request.getContextPath());
-		
+
 		String valor_nombre = request.getParameter("Usuario");
 		String valor_contraseña = request.getParameter("pass");
-		
+
 		System.out.println(valor_nombre + " _ " + valor_contraseña);
-		
-		if(valor_nombre != null && valor_contraseña != null) {
-			if(valor_nombre.equals("Manuel") && valor_contraseña.equals("pass")) {
+
+		if (valor_nombre != null && valor_contraseña != null) {
+			if (valor_nombre.equals("Manuel") && valor_contraseña.equals("pass")) {
 				request.setAttribute("entrar", "sí");
 				request.setAttribute("Usuario", valor_nombre);
 				System.out.println("OK! GET");
-			}else {
+			} else {
 				request.setAttribute("error", "sí");
 			}
 		}
-		
+
 		request.getRequestDispatcher("/index.jsp").forward(request, response);
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		String valor_nombre;
 		String valor_contraseña;
-		
+
 //		BufferedReader reader = request.getReader();
 //		String rawData = reader.readLine();
 //		System.out.println("Datos en bruto con getReader(): " + rawData);
@@ -59,22 +63,23 @@ public class Formulario extends HttpServlet {
 //		String valor_nombre = rawData.substring(rawData.indexOf("Usuario=")+8, rawData.indexOf("&pass"));
 //		String valor_contraseña = rawData.substring(rawData.indexOf("pass=")+5, rawData.indexOf("&Boton"));
 //		System.out.println(valor_nombre + " _ " + valor_contraseña);
-		//Si los parametros son leídos antes por getReader, luego no están disponibles con getParameter().
-		
+		// Si los parametros son leídos antes por getReader, luego no están disponibles
+		// con getParameter().
+
 		valor_nombre = request.getParameter("Usuario");
 		valor_contraseña = request.getParameter("pass");
 		System.out.println(valor_nombre + " _ " + valor_contraseña);
-		
-		if(valor_nombre != null && valor_contraseña != null) {
-			if(valor_nombre.equals("Manuel") && valor_contraseña.equals("pass")) {
+
+		if (valor_nombre != null && valor_contraseña != null) {
+			if (valor_nombre.equals("Manuel") && valor_contraseña.equals("pass")) {
 				request.setAttribute("entrar", "sí");
 				request.setAttribute("Usuario", valor_nombre);
 				System.out.println("OK! POST");
-			}else {
+			} else {
 				request.setAttribute("error", "sí");
 			}
 		}
-		
+
 		request.getRequestDispatcher("/index.jsp").forward(request, response);
 	}
 }
--- PRIMER_PROYECTO_WEB/src/main/webapp/index.jsp
+++ PRIMER_PROYECTO_WEB/src/main/webapp/index.jsp
@@ -1,34 +1,34 @@
-<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <title>Inicio de sesión</title>
-
 <style type="text/css">
-form{
-    display: flex;
-    flex-flow: column;
-    margin-top: 3em;
-}
+	form{
+	    display: flex;
+	    flex-flow: column;
+	    margin-top: 3em;
+	}
 </style>
 </head>
-<body>
-    <%if (request.getAttribute("entrar") == null){%>
-		<form action="<%=request.getContextPath()%>/Formulario" method="get">
-	        Usuario:<input type="text" name="Usuario" placeholder="Escribe tu usuario."></input>
-	        Contraseña:<input type="password" name="pass"></input>
-	        <input type="submit" name="BotonEnviar" value="Enviar por Get"></input>
-	    </form>
-	    
-	    <form action="<%=request.getContextPath()%>/Formulario" method="post">
-	        Usuario:<input type="text" name="Usuario" placeholder="Escribe tu usuario."></input>
-	        Contraseña:<input type="password" name="pass"></input>
-	        <input type="submit" name="BotonEnviar" value="Enviar por Post"></input>
-	    </form>
-	    <%if (request.getAttribute("error") != null){%><span>Ha ocurrido un error, vuelve a intentarlo.</span><%}%>
-     <%}else{%>
-        Hola <%=request.getAttribute("Usuario")%>!!!
-     <%}%>
-</body>
+	<body>
+	    <%if (request.getAttribute("entrar") == null){%>
+			<form action="<%=request.getContextPath()%>/Formulario" method="get">
+		        Usuario:<input type="text" name="Usuario" placeholder="Escribe tu usuario."></input>
+		        Contraseña:<input type="password" name="pass"></input>
+		        <input type="submit" name="BotonEnviar" value="Enviar por Get"></input>
+		    </form>
+		    
+		    <form action="<%=request.getContextPath()%>/Formulario" method="post">
+		        Usuario:<input type="text" name="Usuario" placeholder="Escribe tu usuario."></input>
+		        Contraseña:<input type="password" name="pass"></input>
+		        <input type="submit" name="BotonEnviar" value="Enviar por Post"></input>
+		    </form>
+		    <%if (request.getAttribute("error") != null){%><span>Ha ocurrido un error, vuelve a intentarlo.</span><%}%>
+	     <%}else{%>
+	        Hola <%=request.getAttribute("Usuario")%>!!!
+	     <%}%>
+	</body>
 </html>
